23aac72a223f4850fe8c8b4d5e577fdc37ccaf87
med.wf <med.wf95@gmail.com>
2024-04-24 16:55:25 +0100

    add task 2 - GROUP origin aith sum of funs 👍

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
+
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+GROUP BY origin;
