a12679f11f0b6a121415667b990baf44523aa1b6
med.wf <med.wf95@gmail.com>
2024-04-24 15:55:11 +0100

    add new repo - start project SQL advanced - task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- this script create a table users.
+-- table have (id: int, email: string(255), name: string(255))
+
+CREATE TABLE IF NOT EXISTS `users`(
+    `id` INT PRIMARY KEY,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    `name` VARCHAR(255)
+);
