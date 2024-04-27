2596e16ab2faa819fbbd7b401ca16a65becae1a6
med.wf <med.wf95@gmail.com>
2024-04-27 19:50:05 +0100

    add task 100 - compute AVG using weight - Woow 😲

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,18 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER ##
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN userId INT)
+BEGIN
+    DECLARE total_weight INT;
+    SELECT SUM(weight) INTO total_weight FROM projects;
+
+    UPDATE users
+    SET average_score = (
+        SELECT (SUM(corrections.score * projects.weight) / total_weight)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = userId
+    )
+    WHERE users.id = userId;
+END ##
+DELIMITER ;
