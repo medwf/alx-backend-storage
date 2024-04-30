bbef80c1acb48c752bd195bcb0879ea811dfc8c5
med.wf <med.wf95@gmail.com>
2024-04-30 06:54:39 +0100

    add task 8 - error require i dont now why

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """this module for task 8."""
-from pymongo.collection import Collection
-from typing import List
+# from pymongo.collection import Collection
+# from typing import List
 
 
-def list_all(mongo_collection: Collection) -> List[dict]:
+def list_all(mongo_collection):
     """
     function that lists all documents in a collection.
     Args:
